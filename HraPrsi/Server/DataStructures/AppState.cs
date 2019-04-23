e3762f5150c9b19df1cbbbc3c8c90992900c08c4
Buš Ondřej (195821) <xbuson01@vutbr.cz>
2019-04-23 18:18:16 +0200

    Card stacks

--- HraPrsi/Server/DataStructures/AppState.cs
+++ HraPrsi/Server/DataStructures/AppState.cs
@@ -4,9 +4,11 @@ namespace Server.DataStructures
 {
     public class AppState
     {
-        public bool gameStarted = false;
         public List<Player> players = new List<Player>();
+        public List<Card> cardStack = new List<Card>();
+        public Card topCardStackPlayed = new Card();
 
+        public bool gameStarted = false;
         public int playerTurn = 0;
     }
 }

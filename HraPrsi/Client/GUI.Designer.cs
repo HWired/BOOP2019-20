539dcf582e53644c127c3f9efe2185985e953add
Edesak <siegel.marek@gmail.com>
2019-04-24 16:13:14 +0200

    Základ GUI 1. Přemístění karty pomocí tlačítka 2. Všechny karty jsou v Resources včetně Testovací karty

---
+++ HraPrsi/Client/GUI.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Client
+{
+    partial class GUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.testkarta = new System.Windows.Forms.PictureBox();
+            this.Hrajkartu = new System.Windows.Forms.Button();
+            this.Tahnikartu = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.testkarta)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // testkarta
+            // 
+            this.testkarta.AccessibleDescription = "testkarta";
+            this.testkarta.AccessibleName = "testkarta";
+            this.testkarta.Image = global::Client.Properties.Resources.TestKarty;
+            this.testkarta.Location = new System.Drawing.Point(644, 522);
+            this.testkarta.Name = "testkarta";
+            this.testkarta.Size = new System.Drawing.Size(160, 180);
+            this.testkarta.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.testkarta.TabIndex = 1;
+            this.testkarta.TabStop = false;
+            this.testkarta.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // Hrajkartu
+            // 
+            this.Hrajkartu.Location = new System.Drawing.Point(1329, 625);
+            this.Hrajkartu.Name = "Hrajkartu";
+            this.Hrajkartu.Size = new System.Drawing.Size(125, 77);
+            this.Hrajkartu.TabIndex = 2;
+            this.Hrajkartu.Text = "Hraj kartu";
+            this.Hrajkartu.UseVisualStyleBackColor = true;
+            this.Hrajkartu.Click += new System.EventHandler(this.Hrajkartu_Click);
+            // 
+            // Tahnikartu
+            // 
+            this.Tahnikartu.Location = new System.Drawing.Point(12, 625);
+            this.Tahnikartu.Name = "Tahnikartu";
+            this.Tahnikartu.Size = new System.Drawing.Size(128, 77);
+            this.Tahnikartu.TabIndex = 3;
+            this.Tahnikartu.Text = "Tahni Kartu";
+            this.Tahnikartu.UseVisualStyleBackColor = true;
+            this.Tahnikartu.Click += new System.EventHandler(this.Tahnikartu_Click);
+            // 
+            // GUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1466, 714);
+            this.Controls.Add(this.Tahnikartu);
+            this.Controls.Add(this.Hrajkartu);
+            this.Controls.Add(this.testkarta);
+            this.Name = "GUI";
+            this.Text = "GUI";
+            ((System.ComponentModel.ISupportInitialize)(this.testkarta)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox testkarta;
+        private System.Windows.Forms.Button Hrajkartu;
+        private System.Windows.Forms.Button Tahnikartu;
+    }
+}
---
+++ HraPrsi/Client/GUI.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class GUI : Form
+    {
+        private JPEG Imagine;
+        public GUI()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Hrajkartu_Click(object sender, EventArgs e)
+        {
+
+            testkarta.Location = new Point (655,230);
+            
+        }
+        private void SelectCard()
+        {
+
+        }
+
+        private void Tahnikartu_Click(object sender, EventArgs e)
+        {
+           PictureBox Karta = new PictureBox();
+            Karta.Image = Client.Properties.Resources.KartaBack;
+          
+        }
+        
+    }
+}

5aaa7c1b1fad3f2d9622e425182f282450429f0c
Buš Ondřej (195821) <xbuson01@vutbr.cz>
2019-04-29 16:30:57 +0200

    Fixed namespaces Client.Networking -> Networking

--- HraPrsi/Client/Form1.cs
+++ HraPrsi/Client/Form1.cs
@@ -8,19 +8,18 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-using Client.Networking;
 
 namespace Client
 {
     public partial class Form1 : Form
     {
-        Networking.Networking networking;
+        Networking networking;
 
         public Form1()
         {
             InitializeComponent();
 
-            networking = new Networking.Networking();
+            networking = new Networking();
         }
 
         private void NewGameBtn_Click(object sender, EventArgs e)

9957918dab176d21a8cc6e75739409b3a877e01a
Buš Ondřej (195821) <xbuson01@vutbr.cz>
2019-05-08 19:00:08 +0200

    Validation of player, session name

--- HraPrsi/Client/StartingGUI.cs
+++ HraPrsi/Client/StartingGUI.cs
@@ -27,6 +27,13 @@ namespace Client
         private void NewGameBtn_Click(object sender, EventArgs e)
         {
             string playerName = UserNameInput.Text;
+
+            if (!IsPlayerNameValid(playerName))
+            {
+                UserNameInput.Text = "Enter your name";
+                return;
+            }
+            
             networking.CreateSession(playerName);
             SessionCodeInput.Text = networking.sessionName;
 
@@ -35,12 +42,38 @@ namespace Client
 
         private void JoinGameBtn_Click(object sender, EventArgs e)
         {
-            
-            string sessionName = SessionCodeInput.Text;
             string playerName = UserNameInput.Text;
-            networking.JoinSession(sessionName, playerName);
+            if (!IsPlayerNameValid(playerName))
+            {
+                UserNameInput.Text = "Enter your name";
+                return;
+            }
 
+            string sessionName = SessionCodeInput.Text;
+            if (!IsSessionNameValid(sessionName))
+            {
+                SessionCodeInput.Text = "Enter the session code";
+                return;
+            }
+            
+            networking.JoinSession(sessionName, playerName);
             RunGame();
+        }
+
+        private bool IsSessionNameValid (string sessionName)
+        {
+            if (sessionName != "" && sessionName != "Enter the session code")
+                return true;
+
+            return false;
+        }
+
+        private bool IsPlayerNameValid(string playerName)
+        {
+            if (playerName != "" && playerName != "Enter your name")
+                return true;
+
+            return false;
         }
 
         private void RunGame ()
